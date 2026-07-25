7e2c9078cf5242040bdf9e4422822cc208496d46
Karen Spriggs <k.spriggs01@gmail.com>
2026-07-25 16:14:17 -0500

    navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
-import React from 'react';
+import Navbar from "./components/Navbar.jsx";
 
-function App(props) {
+const App = () => {
     return (
-        <div></div>
-    );
+        <main>
+            <Navbar />
+        </main>
+    )
 }
 
 export default App;
--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from "@tailwindcss/vite";
-import {resolve, dirname} from 'path'
-import {fileURLtoPath} from 'url'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  resolve: {
-    alias: {
-      '#components': resolve(dirname(fileURLtoPath(import.meta.url)), 'components'),
-      '#constants': resolve(dirname(fileURLtoPath(import.meta.url)), 'constants'),
-      '#store': resolve(dirname(fileURLtoPath(import.meta.url)), 'store'),
-      '#hoc': resolve(dirname(fileURLtoPath(import.meta.url)), 'hoc'),
-      '#windows': resolve(dirname(fileURLtoPath(import.meta.url)), 'windows'),
-    }
-  }
 })
