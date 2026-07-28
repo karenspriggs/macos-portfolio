e004cfb90a9a3a396fee25019e75ee70cf8f1ead
Karen Spriggs <k.spriggs01@gmail.com>
2026-07-28 18:06:13 -0500

    zustand window state

--- src/components/Dock.jsx
+++ src/components/Dock.jsx
@@ -3,8 +3,10 @@ import {dockApps} from "../constants/index.js";
 import {Tooltip} from "react-tooltip";
 import {useGSAP} from "@gsap/react";
 import gsap from "gsap";
+import useWindowStore from "../store/window.js";
 
 const Dock = () => {
+    const {openWindow, closeWindow, windows } = useWindowStore()
     const dockRef = useRef(null);
 
     useGSAP(() => {
@@ -53,7 +55,22 @@ const Dock = () => {
     }, [])
 
     const toggleApp = (app) => {
+        if(!app.canOpen) return;
 
+        const window = windows[app.id];
+
+        if (!window) {
+            console.error(`Window not found for app ${app.id}`);
+            return;
+        }
+
+        if (window.isOpen){
+            closeWindow(app.id);
+        } else {
+            openWindow(app.id);
+        }
+
+        console.log(windows);
     }
 
     return (
